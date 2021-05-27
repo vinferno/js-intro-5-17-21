613216eea33a7a8eeb15a08143ede48a7d6f39e9
geekwisevinson <geekwise.vinson.fernandez@gmail.com>
2021-05-26 19:48:13 -0700

    day4

---
+++ day-04/js/overview.js
@@ -0,0 +1,3 @@
+document.body.appendChild(document.createElement('input'));
+const input = document.querySelector('input');
+console.log(input);
---
+++ day-04/js/truthy-falsey.js
@@ -0,0 +1,28 @@
+// const yes = true; // = assignment operator (truthy)
+const no = false; // boolean (falsey)
+
+// const abc = 'abc';
+// const name = 'Vinson';
+// const title = 'title';
+// const red = 'red';
+const falseyString = '';
+
+// const numbers  = -1;
+// const number2 = 2.5;
+const falseyNumber = 0;
+let example;
+
+
+// let loggedInUser = {};
+loggedInUser = NaN;
+
+const myNull = null;
+const myUndefined = undefined;
+// const forever = Infinity;
+
+
+
+
+
+
+
