83cce1a84278290a0b840413e065fd934c0888d0
geekwisevinson <geekwise.vinson.fernandez@gmail.com>
2021-06-14 20:07:22 -0700

    add content type

--- day-09/js/main.js
+++ day-09/js/main.js
@@ -1,16 +1,42 @@
 
-function api(endpoint = '') {
-    return fetch('https://60e2c52d7d58.ngrok.io/' + endpoint)
+function api(endpoint = '', data) {
+    return fetch('https://60e2c52d7d58.ngrok.io/' + endpoint, {
+        method: data ? "POST" : "GET",
+        body: data,
+        headers: {'content-type': 'application/json'},
+    })
     .then(function (res) {
         return res.json();
     })
 }
 
 const getUsersButton = document.querySelector('#getUsers');
+const sendUserNameInput = document.querySelector('#sendUserNameInput');
+const sendUserButton = document.querySelector('#sendUserButton');
+
+
+sendUserButton.innerText = 'Send User';
+
 
 getUsersButton.addEventListener( 'click', function()  {
     api('users')
     .then( (data) => data.users )
     .then( function(users) { console.log(users) });
+});
+
+sendUserButton.addEventListener( 'click', () => {
+    const username = sendUserNameInput.value;
+    console.log(username);
+
+    if (!username) {
+        return;
+    }
+
+
+
+    console.log('this wont happen unless there is a username', username);
+   
+
+    api('users', JSON.stringify({user: username}) ).then( data => console.log('post', data));
 });
 
